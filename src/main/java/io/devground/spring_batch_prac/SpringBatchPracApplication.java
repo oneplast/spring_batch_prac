0276a2c38a4b7f0b4ef1ac38dbac63279ab3f059
Gyeonghun Min <riverpower6@naver.com>
2026-01-24 23:36:42 +0900

    [SETTING] 스프링 배치 사용 프로젝트 실습 세팅

---
+++ src/main/java/io/devground/spring_batch_prac/SpringBatchPracApplication.java
@@ -0,0 +1,13 @@
+package io.devground.spring_batch_prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchPracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/devground/spring_batch_prac/SpringBatchPracApplicationTests.java
@@ -0,0 +1,13 @@
+package io.devground.spring_batch_prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
