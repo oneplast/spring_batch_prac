d614411ed0a369378cd3686676c3f5b9ada48351
Gyeonghun Min <riverpower6@naver.com>
2026-01-28 11:12:21 +0900

    [FEAT] SecurityUser 사용하는 UserDetailsService 구현체 구현

---
+++ src/main/java/io/devground/spring_batch_prac/global/security/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package io.devground.spring_batch_prac.global.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import io.devground.spring_batch_prac.domain.member.member.entity.Member;
+import io.devground.spring_batch_prac.domain.member.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class CustomUserDetailsService implements UserDetailsService {
+
+	private final MemberRepository memberRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Member member = memberRepository.findByUsername(username)
+			.orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+
+		return new SecurityUser(
+			member.getId(),
+			member.getUsername(),
+			member.getPassword(),
+			member.getAuthorities()
+		);
+	}
+}
