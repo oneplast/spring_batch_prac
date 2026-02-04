c7c188182c14267eca14bad4968729733740d34e
Gyeonghun Min <riverpower6@naver.com>
2026-02-04 15:19:19 +0900

    [REFACTOR][BE] GlobalExceptionHandler 도입으로 에러를 잡아서 우아하게 처리

---
+++ src/main/java/io/devground/spring_batch_prac/global/exception/GlobalCommonExceptionHandler.java
@@ -0,0 +1,20 @@
+package io.devground.spring_batch_prac.global.exception;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import io.devground.spring_batch_prac.global.rq.Rq;
+import lombok.RequiredArgsConstructor;
+
+@ControllerAdvice(annotations = Controller.class)
+@RequiredArgsConstructor
+public class GlobalCommonExceptionHandler {
+
+	private final Rq rq;
+
+	@ExceptionHandler(GlobalException.class)
+	public String handle(GlobalException ex) {
+		return rq.historyBack(ex.getRsData().getMsg());
+	}
+}
