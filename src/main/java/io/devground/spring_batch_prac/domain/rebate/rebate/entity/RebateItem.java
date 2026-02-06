64a52975b21c7041f41f4717afe1a8a6f48cc6e3
GyeongHun <riverpower6@naver.com>
2026-02-06 15:27:51 +0900

    [REFACTOR][BE] 정산 데이터의 연관관계 관련 제약조건 해제
    1. 정산 데이터의 경우 대용량 트래픽 발생 가능
      - 특히 @OneToOne의 경우, DDL에 Unique + FK 제약이 함께 생성되어, INSERT 시 무결성 검사 비용 증가
      - 정산의 경우 TPS가 높고 INSERT가 많으므로, 병목 현상 발생 가능
    2. 외래키 제약조건으로 인한 운영 난이도 증가
      - ex) Product를 삭제하려면 해당 id를 FK로 가진 RebateItem 먼저 삭제필요
    3. 어플리케이션에서 정합성을 맞추고, 해당 도메인 관련 성능 개선
      - 정산은 과거 데이터이고 수정 거의 불필요
      - UK를 제거하여 성능 개선 및 병목 현상 방지

--- src/main/java/io/devground/spring_batch_prac/domain/rebate/rebate/entity/RebateItem.java
+++ src/main/java/io/devground/spring_batch_prac/domain/rebate/rebate/entity/RebateItem.java
@@ -1,5 +1,7 @@
 package io.devground.spring_batch_prac.domain.rebate.rebate.entity;
 
+import static jakarta.persistence.ConstraintMode.*;
+
 import java.time.LocalDateTime;
 
 import io.devground.spring_batch_prac.domain.member.member.entity.Member;
@@ -7,6 +9,8 @@ import io.devground.spring_batch_prac.domain.product.order.entity.OrderItem;
 import io.devground.spring_batch_prac.domain.product.product.entity.Product;
 import io.devground.spring_batch_prac.global.jpa.BaseTime;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
@@ -27,14 +31,18 @@ import lombok.ToString;
 public class RebateItem extends BaseTime {
 
 	@OneToOne
+	@JoinColumn(foreignKey = @ForeignKey(NO_CONSTRAINT))
 	private OrderItem orderItem;
 
 	@ManyToOne
+	@JoinColumn(foreignKey = @ForeignKey(NO_CONSTRAINT))
 	private Member seller;
 	@ManyToOne
+	@JoinColumn(foreignKey = @ForeignKey(NO_CONSTRAINT))
 	private Member buyer;
 
 	@ManyToOne
+	@JoinColumn(foreignKey = @ForeignKey(NO_CONSTRAINT))
 	private Product product;
 	private long payPrice;
 	private LocalDateTime eventDate;
